fb56fd8fa12c1f0f5e735a99016288b3fef96e49
NeeluSan <neelu_sankaran@yahoo.co.in>
2018-11-06 14:21:29 -0800

    Update image.php

--- image.php
+++ image.php
@@ -13,7 +13,7 @@ if ( $deployment == 'blue') {
   $color = imagecolorallocate($im, 0, 0, 255);
 }
 
-// Draw a filled rectangle
+// Draw a filled rectangle, add sample comment here
 imagefilledrectangle($im, 0, 0, 199, 199, $color);
 
 // Save the image
